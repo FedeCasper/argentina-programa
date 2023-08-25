6a34c4669eaebd38c2f229165e080b6b13d84f07
FedeCasper <federicorouyere@gmail.com>
2023-08-25 10:55:38 -0300

    clase de condicionales

---
+++ AP-Desarrollo-Web-14TN/condicionales/condicionales/index.js
@@ -0,0 +1,57 @@
+//IF
+
+// var edad = 40;
+// if ( edad < 30){
+//     console.log ("la edad es menor a 30")
+// } else {
+//     console.log("la edad es mayor a 30")
+// }
+
+
+//IF ELSE
+// let nombre = "Flor"
+// let edad = 16
+
+// if(edad >= 18){
+//     console.log (nombre + " "+ "puede hacer el curso de AP")
+// }else {
+//     console.log(nombre + " " + "no puede hacer el curso de AP")
+// }
+
+
+//ELSE IF
+// const numero = -10
+// if(numero > 0){
+// console.log(numero + " "+ "Es positivo")
+// } else if  (numero < 0){
+// console.log(numero + " " + "Es negativo")
+// } else {
+//     console.log(numero + " " + "es 0")
+// }
+
+
+//SWITCH
+// let x = 10
+// let y = 5
+// let operacion = "multiplicar"
+
+// switch(operacion){
+//     case "sumar":
+//         console.log(x + y)
+//         break
+
+//         case "restar":
+//         console.log(x - y)
+//         break
+
+//         case "dividir":
+//         console.log(x / y)
+//         break
+
+//         case "multiplicar":
+//         console.log(x * y)
+//         break
+//         default:
+//             console.log("operacion no válida")
+
+// }
